445488b38aef81c59b8c39750d9b7732e5f6ef0a
Preeti Krishnan <peakzzz@gmail.com>
2016-11-19 18:39:30 -0800

    Update rerun.sh

--- rerun.sh
+++ rerun.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-BASEDIR=/user/user01/LAB1/E1
+BASEDIR=/user/user01/
 
 export CLASSPATH=$(hadoop classpath)
 export HADOOP_CLASSPATH=$CLASSPATH
 
-rm -rf $BASEDIR/OUT 
-hadoop jar Dictionary.jar Dictionary.DictionaryDriver $BASEDIR/DATA $BASEDIR/OUT
+rm -rf $BASEDIR/OUT
+hadoop jar Dictionary.jar Dictionary.DictionaryDriver $BASEDIR/INPUT $BASEDIR/OUT
